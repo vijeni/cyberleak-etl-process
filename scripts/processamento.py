d36a4db4468e8de340da87b2e661e464cbd5e614
Jean Buss <jean.buss@itaipuparquetec.org.br>
2024-11-12 20:34:39 -0300

    v3 - threads beta

--- scripts/processamento.py
+++ scripts/processamento.py
@@ -1,12 +1,12 @@
 import os
 import re
 from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
 from db.db_operacoes import (
     inserir_dim_tempo,
     inserir_dim_dominio,
     inserir_dim_email,
     inserir_dim_senha,
-    inserir_fato_fonte_vazamento,
     inserir_fato_vazamentos_email,
     inserir_fato_vazamentos_senha
 )
@@ -21,7 +21,64 @@ barra_progresso_custom = (
 )
 
 
-def processar_arquivo(db, caminho_arquivo, separador):
+def processar_chunk(chunk, db, id_tempo, separador, logger, barra):
+    contador_linhas = 0
+    for linha in chunk:
+        linha = linha.replace('\x00', '').strip()
+        if not linha:
+            continue
+
+        contador_linhas += 1
+        if separador == ':':
+            if "|" in linha:
+                partes = linha.split("|")
+                if len(partes) != 3:
+                    logger.warning(
+                        f"Erro ao processar linha (formato inesperado): {linha}")
+                    continue
+                url, usuario, senha = partes
+            else:
+                partes = linha.rsplit(separador, 2)
+                if len(partes) != 3:
+                    logger.warning(
+                        f"Formato inválido (componentes insuficientes): {linha}")
+                    continue
+                url, usuario, senha = partes
+        else:
+            partes = linha.split(separador)
+            if len(partes) != 3:
+                logger.warning(
+                    f"Erro ao processar linha (formato inesperado): {linha}")
+                continue
+            url, usuario, senha = partes
+
+        # Validar URL e Senha
+        match = re.match(r'^(https?):\/\/[^:]+(?::\d+)?@?[^:]*', url)
+        if not match:
+            logger.warning(f"Formato inválido (URL): {linha}")
+            continue
+        url = match.group(0)
+
+        if not senha or len(senha) > 40 or len(senha) < 4:
+            logger.warning(f"Senha inválida: {senha}")
+            continue
+
+        # Inserção no banco de dados
+        id_dominio = inserir_dim_dominio(db, extrair_dominio(url))
+        id_senha = inserir_dim_senha(db, senha)
+        inserir_fato_vazamentos_senha(db, id_tempo, id_dominio, id_senha)
+
+        if email_valido(usuario):
+            id_email = inserir_dim_email(db, usuario)
+            inserir_fato_vazamentos_email(db, id_tempo, id_dominio, id_email)
+
+        # Atualiza a barra de progresso para cada linha processada
+        barra.update(1)
+
+    return contador_linhas
+
+
+def processar_arquivo(db, caminho_arquivo, separador, max_workers=80, chunk_size=1000):
     data, plataforma_origem = extrair_dados_arquivo(
         os.path.basename(caminho_arquivo))
     if not data or not plataforma_origem:
@@ -29,108 +86,39 @@ def processar_arquivo(db, caminho_arquivo, separador):
 
     logger = configure_logging(plataforma_origem)
     id_tempo = inserir_dim_tempo(db, data)
-    # id_fonte_vazamento = inserir_fato_fonte_vazamento(db, plataforma_origem)
 
     with open(caminho_arquivo, 'r', encoding='utf-8') as arquivo:
         quantidade_linhas = sum(1 for _ in arquivo)
         arquivo.seek(0)
 
-        contador_linhas = 0
+        # Se o arquivo for CSV, ignorar o cabeçalho
+        if caminho_arquivo.lower().endswith('.csv'):
+            next(arquivo)
 
-        with tqdm(total=quantidade_linhas, desc=Fore.BLUE + "Processando" + Style.RESET_ALL, bar_format=barra_progresso_custom,
-                  ascii=False, unit=" linha") as barra:
-            if caminho_arquivo.lower().endswith('.csv'):
-                next(arquivo)
+        # Configura a barra de progresso
+        with tqdm(total=quantidade_linhas, desc=Fore.BLUE + "Processando" + Style.RESET_ALL,
+                  bar_format=barra_progresso_custom, ascii=False, unit=" linha") as barra:
 
-            for linha in arquivo:
-                contador_linhas += 1
-                barra.update(1)
+            # Executor para processar cada chunk em threads paralelas
+            with ThreadPoolExecutor(max_workers=max_workers) as executor:
+                futures = []
+                chunk = []
 
-                linha = linha.replace('\x00', '').strip()
+                for linha in arquivo:
+                    chunk.append(linha)
+                    if len(chunk) >= chunk_size:
+                        # Processa um chunk em uma thread
+                        futures.append(executor.submit(
+                            processar_chunk, chunk, db, id_tempo, separador, logger, barra))
+                        chunk = []
 
-                if not linha:
-                    continue
+                # Processa o último chunk (se não estiver vazio)
+                if chunk:
+                    futures.append(executor.submit(
+                        processar_chunk, chunk, db, id_tempo, separador, logger, barra))
 
-                if separador == ':':
-                    if "|" in linha:
-                        partes = linha.split("|")
-                        if len(partes) == 3:
+                # Aguarda todas as threads completarem
+                total_linhas = sum(f.result() for f in futures)
+                print(f"Total de linhas processadas: {total_linhas}")
 
-                            url, usuario, senha = partes
-
-                            # Regex para capturar URLs (http, https, android) e lidar com portas
-                            match = re.match(
-                                r'^(https?):\/\/[^:]+(?::\d+)?@?[^:]*', url)
-                            if match:
-                                url = match.group(0)
-
-                            else:
-                                logger.warning(
-                                    f"Formato inválido (URL): {linha}")
-                                continue
-                        else:
-                            logger.warning(
-                                f"Erro ao processar linha (formato inesperado): {linha}")
-                            continue
-                        url, usuario, senha = partes
-                    else:
-                        partes = linha.rsplit(separador, 2)
-
-                        if len(partes) == 3:
-                            url, usuario, senha = partes
-
-                            # Regex para capturar URLs (http, https, android) e lidar com portas
-                            match = re.match(
-                                r'^(https?):\/\/[^:]+(?::\d+)?@?[^:]*', url)
-                            if match:
-                                url = match.group(0)
-
-                            else:
-                                logger.warning(
-                                    f"Formato inválido (URL): {linha}")
-                                continue
-                        else:
-                            logger.warning(
-                                f"Formato inválido (componentes insuficientes): {linha}")
-                            continue
-                else:
-                    partes = linha.split(separador)
-                    if len(partes) == 3:
-                        url, usuario, senha = partes
-
-                        # Regex para capturar URLs (http, https, android) e lidar com portas
-                        match = re.match(
-                            r'^(https?):\/\/[^:]+(?::\d+)?@?[^:]*', url)
-                        if match:
-                            url = match.group(0)
-
-                        else:
-                            logger.warning(
-                                f"Formato inválido (URL): {linha}")
-                            continue
-                    else:
-                        logger.warning(
-                            f"Erro ao processar linha (formato inesperado): {linha}")
-                        continue
-
-                    url, usuario, senha = partes
-
-                if not senha:
-                    logger.warning(f"Usuário ou senha vazios: {linha}")
-                    continue
-                if len(senha) > 40 or len(senha) < 4:
-                    logger.warning(
-                        f"Senha não possui entre 4 e 40 caracteres: {senha} - {len(senha)}")
-                    continue
-
-                id_dominio = inserir_dim_dominio(db, extrair_dominio(url))
-                id_senha = inserir_dim_senha(db, senha)
-                inserir_fato_vazamentos_senha(
-                    db, id_tempo, id_dominio, id_senha)
-
-                if email_valido(usuario):
-                    id_email = inserir_dim_email(db, usuario)
-                    inserir_fato_vazamentos_email(
-                        db, id_tempo, id_dominio, id_email)
-
-    return contador_linhas
+    return total_linhas
